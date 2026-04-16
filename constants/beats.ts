39b8009e1ca462ea2cd093b2561cda435be7616c
Rivoarivelo <rrivorakotoniaina1@gmail.com>
2026-04-16 21:04:54 +0200

    The beat

--- constants/beats.ts
+++ constants/beats.ts
@@ -1,5 +1,5 @@
 export const BEATS = [
-  { id: 1, name: "Afrobeat PARADO", bpm: 100, duration: "3:20", note: "G", file: "/audio/Instrumental Afrobeat 2026 PARADO.wav", cover: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=300&h=300&fit=crop",genre : "Afrobeat" },
-  { id: 2, name: "UK DRILL 2026", bpm: 135, duration: "2:45", note: "F#m", file: "/Instrumental UkDrill 2026 UK.wav", cover: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?w=300&h=300&fit=crop", genre : "Drill" },
+  { id: 1, name: "Afrobeat PARADO", bpm: 100, duration: "3:20", note: "G", file: "/audio/Instrumentals Afrobeat 2026 PARADO.wav", cover: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=300&h=300&fit=crop",genre : "Afrobeat" },
+  { id: 2, name: "UK DRILL 2026", bpm: 135, duration: "2:45", note: "F#m", file: "/Instrumentals UkDrill 2026 UK.wav", cover: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?w=300&h=300&fit=crop", genre : "Drill" },
   { id: 3, name: "Afro Queen", bpm: 105, duration: "3:10", note: "C", file: "/audio/afro-queen.mp3", cover: "https://images.unsplash.com/photo-1514525253344-9914f2e8d695?w=300&h=300&fit=crop" },
 ];
